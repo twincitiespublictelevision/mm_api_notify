02483c57ddd1ac00edc6b183ac9fd6f8ba8a439d
johnsonpaul1014 <pjohnson@tpt.org>
2016-11-13 22:16:55 -0600

    A few more compile borrow errors to fix

--- src/video.rs
+++ src/video.rs
@@ -6,7 +6,7 @@ extern crate serde_json;
 use super::worker_pool;
 use std::thread;
 use std::sync::Arc;
-use self::serde_json::Value;
+use self::serde_json::{Value, Map};
 
 use super::cove;
 
@@ -23,18 +23,18 @@ pub struct Program<'a> {
 ///
 pub fn ingest(first_time: bool) {
     let total_programs = get_total_programs();
-    let mut updated_date = String::from("");
+    let mut updated_date = Arc::new(String::from(""));
 
     if !first_time {
         let current_time = time::now_utc();
-        let updated_date = format!("{}-{}-{}", current_time.tm_year, current_time.tm_mon, current_time.tm_mday);
+        updated_date = Arc::new(format!("{}-{}-{}", current_time.tm_year, current_time.tm_mon, current_time.tm_mday));
     }
-
+      
     for i in (0..total_programs).step_by(200) {
+        let shared_updated_date = updated_date.clone();
         println!("Processing program set {} of {}", i, total_programs);
         let mut worker_pool = worker_pool::WorkerPool::new();
-        let updated_date = Arc::new(updated_date);
-                  
+               
         worker_pool.join_handles.push(thread::spawn(move || {
             let programs = get_programs(i);
            
@@ -46,7 +46,7 @@ pub fn ingest(first_time: bool) {
                 for j in (0..total_videos).step_by(200) {
                     println!("Processing video set {} of {}", j, total_videos);
                     let shared_program = program.clone();
-                    let shared_updated_date = updated_date.clone();
+                    let shared_updated_date = shared_updated_date.clone();
 
                     worker_pool.join_handles.push(thread::spawn(move || {
                         get_videos(&shared_updated_date, j, &shared_program);
@@ -73,16 +73,16 @@ fn get_total_programs() -> u64 {
 ///
 fn get_programs<'a>(start_index: u64) -> Vec<Program<'a>> {
     let cove_data = cove::video_api("programs", vec![["limit_start", start_index.to_string().as_str()]]);
-    let programs = serde_json::from_value(*cove_data.as_object().unwrap().get("results").unwrap()).unwrap();
-    let programs_data = vec![];
+    let programs: &Vec<Value> = cove_data.as_object().unwrap().get("results").unwrap().as_array().unwrap();
+    let mut programs_data = vec![];
 
     for program in programs {
-        let program_uri = program.as_object().unwrap().get("resource_uri").unwrap().as_str();
-        let program_id = program_uri.split("/")[3].to_u64();
+        let program_uri = program.as_object().unwrap().get("resource_uri").unwrap().as_str().unwrap();
+        let program_id: u64 = program_uri.split("/").nth(3).unwrap().parse().unwrap();
 
         // Do the mongo insert.
 
-        programs_data.push(Program {data: program, program_id: program_id});
+        programs_data.push(Program {data: program.as_str().unwrap().clone(), program_id: program_id});
     }
 
     programs_data
@@ -92,21 +92,22 @@ fn get_programs<'a>(start_index: u64) -> Vec<Program<'a>> {
 /// Gets the total videos for a program so they can be chunked
 ///
 fn get_video_count_for_program<'a>(program: &Program) -> u64 {
-    cove::video_api("videos", vec![["program_id", program.program_id]]).as_object().unwrap().get("count").unwrap().as_u64().unwrap()
+    cove::video_api("videos", vec![["program_id", program.program_id.to_string().as_str()]]).as_object().unwrap().get("count").unwrap().as_u64().unwrap()
 }
 
 ///
 /// Gets all videos from COVE for a program, 200 at a time
 ///
 fn get_videos<'a>(updated_date: &Arc<String>, start_index: u64, program: &Arc<Program>) {
-    let params = vec![["program_id", program.program_id], ["limit_start", start_index.as_str()]];
-
-    if updated_date != "" {
-        params.push(["filter_record_last_updated_datetime__gt", updated_date]);
+    let program_id = program.program_id.to_string().as_str();
+    let mut params = vec![["program_id", program_id], ["limit_start", start_index.to_string().as_str()]];
+    
+    if updated_date.as_str() == "" {
+        params.push(["filter_record_last_updated_datetime__gt", updated_date.as_str()]);
     }
 
     let cove_data = cove::video_api("videos", params);
-    let videos = cove_data.as_object().unwrap().get("results").unwrap();
+    let videos:&Vec<Value> = cove_data.as_object().unwrap().get("results").unwrap().as_array().unwrap();
     
     for video in videos {
 
