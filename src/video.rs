fc37c9bb3f77c167d043d78209587a44b3aa804b
johnsonpaul1014 <pjohnson@tpt.org>
2016-11-16 06:38:20 -0600

    Added deletion of missing videos

--- src/video.rs
+++ src/video.rs
@@ -7,7 +7,7 @@ use super::worker_pool;
 use super::cove;
 
 use std::thread;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 use self::serde_json::Value;
 use self::mongodb::db::{Database, ThreadedDatabase};
 use self::mongodb::coll::options::FindOneAndUpdateOptions;
@@ -46,6 +46,7 @@ pub fn ingest(first_time: bool, db: &Database, num_workers: usize) {
             for program in programs {
                 let program_id = program.program_id;
                 let program_data = program.data.clone();
+                let video_ids = Arc::new(Mutex::new(vec![]));
 
                 let filter = doc! {
                     "program_id" => program_id
@@ -58,28 +59,45 @@ pub fn ingest(first_time: bool, db: &Database, num_workers: usize) {
 
                 let mut options = FindOneAndUpdateOptions::new();
                 options.upsert = true;
-                
                 coll.find_one_and_replace(filter, update, Some(options)).expect("Can't insert program into database!");
+
                 let shared_updated_date = shared_updated_date.clone();
                 let shared_db = shared_db.clone();
+
+                // We need this final shared DB because shared_db is moved into the thread so it can't be used to delete
+                // at the end.
+                let final_shared_db = shared_db.clone();
+
+                let shared_video_ids = video_ids.clone();
                
                 worker_pool.add_worker(thread::spawn(move || {
                     let total_videos = get_video_count_for_program(&shared_updated_date, &program);
                     let mut worker_pool = worker_pool::WorkerPool::new(num_workers);
                     let program = Arc::new(program);
-
+                    let shared_video_ids = shared_video_ids.clone();
+                    
                     for j in (0..total_videos).step_by(200) {
                         let shared_program = program.clone();
                         let shared_updated_date = shared_updated_date.clone();
                         let shared_db = shared_db.clone();
+                        let shared_video_ids = shared_video_ids.clone();
                         
                         worker_pool.add_worker(thread::spawn(move || {
-                            get_videos(&shared_updated_date, j, &shared_program, &shared_db);
+                            get_videos(&shared_updated_date, j, &shared_program, &shared_db, &shared_video_ids);
                         }));
                     }
 
                     worker_pool.wait_for_children();
                 }));
+
+                let video_ids_str = format!("[{}]", video_ids.lock().unwrap().join(","));
+                let filter = doc! {
+                    "tp_media_object_id" => video_ids_str,
+                    "cond" => "$ne"
+                };
+
+                let video_coll = final_shared_db.collection("videos");
+                video_coll.delete_many(filter, None).unwrap();
             }
 
             worker_pool.wait_for_children();
@@ -107,7 +125,7 @@ fn get_programs(start_index: u64) -> Vec<Program> {
     for program in programs {
         let program_uri = program.as_object().unwrap().get("resource_uri").unwrap().as_str().unwrap();
         let program_id: u64 = program_uri.split("/").nth(4).unwrap().parse().unwrap();
-        
+
         programs_data.push(Program {data: program.to_string(), program_id: program_id});
     }
 
@@ -134,7 +152,7 @@ fn get_video_count_for_program<'a>(updated_date: &Arc<String>, program: &Program
 ///
 /// Gets all videos from COVE for a program, 200 at a time
 ///
-fn get_videos<'a>(updated_date: &Arc<String>, start_index: u64, program: &Arc<Program>, db: &Database) {
+fn get_videos(updated_date: &Arc<String>, start_index: u64, program: &Arc<Program>, db: &Database, video_ids: &Arc<Mutex<Vec<String>>>) {
     let program_id = program.program_id.to_string();
     let str_start_index = start_index.to_string();
     let mut params = vec![
@@ -150,6 +168,7 @@ fn get_videos<'a>(updated_date: &Arc<String>, start_index: u64, program: &Arc<Pr
     let cove_data = cove::video_api("videos", params);
     let videos:&Vec<Value> = cove_data.as_object().unwrap().get("results").unwrap().as_array().unwrap();
     let coll = db.collection("videos");
+    let mut page_video_ids = vec![];
 
     for video in videos {
         
@@ -158,7 +177,9 @@ fn get_videos<'a>(updated_date: &Arc<String>, start_index: u64, program: &Arc<Pr
         //
         match video.as_object().unwrap().get("tp_media_object_id").unwrap().as_u64() {
             Some(tp_media_object_id) => {
+                page_video_ids.push(tp_media_object_id.to_string());
                 let video_data = video.to_string();
+                let program_id = program.program_id;
 
                 let filter = doc! {
                     "tp_media_object_id" => tp_media_object_id
@@ -166,6 +187,7 @@ fn get_videos<'a>(updated_date: &Arc<String>, start_index: u64, program: &Arc<Pr
 
                 let update = doc! {
                     "tp_media_object_id" => tp_media_object_id,
+                    "program_id" => program_id,
                     "data" => video_data
                 };
 
@@ -177,4 +199,8 @@ fn get_videos<'a>(updated_date: &Arc<String>, start_index: u64, program: &Arc<Pr
             None => {}
         };
     }
+
+    let shared_video_ids = video_ids.clone();
+    let mut locked_video_ids = shared_video_ids.lock().unwrap();
+    locked_video_ids.extend(page_video_ids);
 }
